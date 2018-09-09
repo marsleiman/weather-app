8579768c867cadf7fa3360865e32101d65d32476
Mariam Sleiman <Mariam@Mariams-MacBook-Air.local>
2018-09-08 23:10:10 -0300

    temperature state

---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,54 @@
+import React, { Component }  from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData/index';
+import {
+    SUN,
+    WINDY,
+} from './../../constants/weather';
+import './styles.css';
+
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+}
+
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '10 m/s',
+}
+
+class WeatherLocation extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data: data,
+        };
+    }
+
+    handelUpdateClick = () => {
+        console.log("actualizado");
+        this.setState = {
+            city: "Buenos Aires!!",
+            data: data2,
+        }
+    }
+
+    render(){
+        const { city, data } = this.state;
+        return(
+            <div className='weatherLocationCont'>
+                <Location city={city}/>
+                <WeatherData data={data}/>
+                <button onClick = {this.handelUpdateClick} >Actualizar</button> 
+            </div>
+        );
+    }    
+}
+
+export default WeatherLocation;
